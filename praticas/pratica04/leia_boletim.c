beb0e4450eadfbc83311bd41cc5f547f592e6cd3
draken <148147503+d0raken@users.noreply.github.com>
2024-09-04 16:30:08 +0000

    Completinho

--- praticas/pratica04/leia_boletim.c
+++ praticas/pratica04/leia_boletim.c
@@ -2,17 +2,23 @@
 
 int main() {
   
-int matricula = 0;
+long int matricula = 0;
 float a1 = 0.0f;
 float a2 = 0.0f;
 float media = 0.0f;
 
 printf("Digite sua matrícula: ");
-int deu_certo = scanf("%i", &matricula);
+int deu_certo = scanf("%li", &matricula);
 printf("Digite a primeia nota: ");
 deu_certo = scanf("%f", &a1);
 printf("Digite a segunda nota: ");
-deu_certo = ("%f", a2);
+deu_certo = scanf("%f", &a2);
+media = (a1 + a2)/2;
+printf("Matricula\t A1\t\t A2\t\t Media\n");
+printf("%li\t %.2f\t %.2f\t %.2f\n", matricula, a1, a2, media);
+
+
+  
 
 
 
