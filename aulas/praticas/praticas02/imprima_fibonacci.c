31670c29be519c49289137fec6279ff40e3a096b
draken <148147503+d0raken@users.noreply.github.com>
2024-11-26 19:01:41 +0000

    deletados

--- aulas/praticas/praticas02/imprima_fibonacci.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-  printf("%10i %-10i\n %8i %i %-10i\n %6i %i %i %i %-10i\n %4i %i %i %i %i %i %-10i\n %2i %i %i %i %i %i %i %i %-10i\n", 1, 1, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 3, 5, 3, 2, 1, 1, 2, 3, 5, 8, 5, 3, 2, 1);
-  
-  return 0;
-
-}
--- aulas/praticas/praticas02/imprima_nota_legal.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  
-  printf("%s\n %20s\n %s\n %s\t\t %s  %s\n %s\t\t %03i\t\t   %.2f" ,"===================================", "NOTA LEGAL","===================================", "Produto","Qtd","Valor Unit", "Camiseta",2, 39.99); 
-  return 0;
-}
